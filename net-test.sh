309b7d0b14fc51109c2ce9629933636bf7afcc0b
nickisabi <revisioneerspodcast@gmail.com>
2023-06-19 23:16:56 -0600

    fully committed

--- net-test.sh
+++ net-test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-touch /home/nickisabi/Documents/net-test.txt
 echo "Would you like to run a test of the network connectivity on your device?"
 read answer
-if [ $answer="yes"] then
+if [ $answer="yes"] then;
+    touch /home/nickisabi/Documents/net-test.txt
     ping 127.0.0.1 >> /home/nickisabi/Documents/net-test.txt
     ping 8.8.8.8 >> /home/nickisabi/Documents/net-test.txt
     ping google.com >> /home/nickisabi/Documents/net-test.txt
     traceroute google.com >> /home/nickisabi/Documents/net-test.txt
-elif [ $answer="no"] then
+elif [ $answer="no" ] then;
     echo "Goodbye!!"
     exit
 else 
